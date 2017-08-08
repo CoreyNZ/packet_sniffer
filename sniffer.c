197c30c7ef4e5e52086cc8ce39aa6059fb69c62a
CoreyNZ <CoreyWilkinsonNZ@gmail.com>
2017-08-09 11:31:04 +1200

    Determined traffic and protocol for IPV4 + Traffic Type for IPV6

--- sniffer.c
+++ sniffer.c
@@ -48,6 +48,11 @@
 
 /* Prototypes */
 void got_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
+void got_ipv6(int, int, const u_char*, char*);
+void print_tcp (const u_char*, int*);
+void print_udp (const u_char*, int*);
+void print_payload (const u_char *, int);
+void print_ipv4(char*, char*);
 
 /* Global Variables */
 int packet_counter = 0;
@@ -93,8 +98,117 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
   int size = 0;
   size += sizeof(struct ether_header);
 
-  printf("Header Length: %d\n", header->len);
+
+  /* Determine the traffic type and protocol type (IPV4)*/
+  switch(ntohs(ethernet_header->ether_type)){
+    case ETHERTYPE_IP:
+      if(ipv4_bool == false) {
+        return;
+      }
+      /* Get IPV4 Header, Source, Destination address, header size */
+      ipv4_header = (struct ip*)(packet + size);
+
+      inet_ntop(AF_INET, &(ipv4_header->ip_src), srcIPV4, INET_ADDRSTRLEN);
+		  inet_ntop(AF_INET, &(ipv4_header->ip_dst), destIPV4, INET_ADDRSTRLEN);
+
+      size += sizeof(struct ip);
+
+      /* Payload */
+      u_char *payload;
+		  int dataLength = 0;
+
+        /* Determine protocol type of IPV4 header */
+        switch(ipv4_header->ip_p){
+
+          case IPPROTO_TCP:
+			       if(tcp_bool == false){
+			            return;
+			       }
+             print_ipv4(srcIPV4, destIPV4);
+   			     print_tcp(packet, &size);
+   			     break;
+
+          case IPPROTO_UDP:
+ 			      if(udp_bool == false){
+ 			          return;
+ 			      }
+ 			      print_ipv4(srcIPV4, destIPV4);
+ 			      print_udp(packet, &size);
+ 			      break;
+
+          case IPPROTO_ICMP:
+  			     if(icmp_bool == false){
+  			          return;
+  			     }
+  			     print_ipv4(srcIPV4, destIPV4);
+  			     printf("Protocol: ICMP \n");
+
+             icmp_header = (struct icmphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
+             u_int type = icmp_header->type;
+
+             if(type == 11){
+			            printf("TTL Expired! \n");
+			       }
+			       else if(type == ICMP_ECHOREPLY){
+			            printf("ICMP Echo Reply! \n");
+			       }
+
+             /* Payload */
+             payload = (u_char*)(packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct icmphdr));
+			       dataLength = header->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct icmphdr));
+			       printf("Payload: (%d bytes) \n", dataLength);
+			       printf("\n");
+			       print_payload(payload, dataLength);
+
+             break;
+
+          /* Unknown Protocol */
+          default:
+            if(unknown_protocol_bool == false){
+              return;
+            }
+            printf("Protocol: Unknown \n");
+            break;
+         }
+     break;
+
+     /* Determine the traffic type (IPV6)*/
+     case ETHERTYPE_IPV6:
+		   if(ipv6_bool == false){
+		    return;
+		   }
+
+       /* Get IPV6 Header, Source, Destination address, header size */
+       ipv6_header = (struct ip6_hdr*)(packet + size);
+
+       inet_ntop(AF_INET6, &(ipv6_header->ip6_src), srcIPV6, INET6_ADDRSTRLEN);
+		   inet_ntop(AF_INET6, &(ipv6_header->ip6_dst), destIPV6, INET6_ADDRSTRLEN);
+       int nextheader = ipv6_header->ip6_nxt;
+
+       size += sizeof(struct ip6_hdr);
+
+       char string[100] = " ";
+
+       got_ipv6(nextheader, size, packet, string);
+       break;
+
+      /* Anon Traffic */
+    default:
+      if(other_traffic_bool == false){
+        return;
+      }
+
+      printf("Ether Type: Other \n");
+      break;
+    }
 }
+
+void got_ipv6(int header, int size, const u_char *packet, char *string)
+{
+  /* Determine the protocol type (IPV6)*/
+  
+}
+
 
 int main(int argc, char **argv)
 {
